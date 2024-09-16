62750b15c70f4647a604e5dc79d7309b161d9372
Shipon Irfan <shiponirfan@Shipons-MacBook-Pro.local>
2024-09-16 17:09:26 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/shiponirfan/todo_crud_app_with_api_m14/todo_crud_app_with_api_m14/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shiponirfan.todo_crud_app_with_api_m14.todo_crud_app_with_api_m14
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo_crud_app_with_api_m14/app.dart';
+
+void main() {
+  runApp(const TodoCrudApp());
+}
